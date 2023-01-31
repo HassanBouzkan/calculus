6d92e12121db95c3c852f2ab4a92913f5100cbed
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-01-31 22:39:17 +0100

    updated template.js

--- template.js
+++ template.js
@@ -2,6 +2,7 @@ const variable1 = 12; const variable2 = 48;
 const result = variable2 / variable1;
 const result1 = variable1 * variable2;
 const result2 = variable1 + variable2;
+const result3 = variable2 - variable1;
 
 console.log("result = "+result);
 console.log("result1 = "+result1);
