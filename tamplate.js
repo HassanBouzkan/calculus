5e8982296ebe0a299f3b499bdb14a318c4944bdd
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-01-31 22:10:40 +0100

    create tamplate.js

---
+++ tamplate.js
@@ -0,0 +1,8 @@
+const variable1 = 12; const variable2 = 48;
+const result = variable2 / variable1;
+const result1 = variable1 * variable2;
+const result2 = variable1 + variable2;
+
+console.log("result = "+result);
+console.log("result1 = "+result1);
+console.log("result2 = "+result2);
