2be3c37700a9c7a194e5d36705d3eff255bcdada
Hassan Bouzkan <bouzkanhassan@gmail.com>
2023-02-01 22:52:23 +0100

    updated template.js

--- template.js
+++ template.js
@@ -1,10 +1,38 @@
-const variable1 = 12; const variable2 = 48;
-const result = variable2 / variable1;
-const result1 = variable1 * variable2;
-const result2 = variable1 + variable2;
-const result3 = variable2 - variable1;
+ //  index :         0        1        2       3       4         5        6        7        8         
+ const colors = ['orange', 'blue', 'yellow', 'red', 'purple', 'green', 'black', 'white', 'pink'];
 
-console.log("result = "+result);
-console.log("result1 = "+result1);
-console.log("result2 = "+result2);
-console.log("result3 = "+result3);
+ const randomIndex = Math.floor(Math.random() * colors.length );
+ 
+ const randomColor = colors[randomIndex];
+ 
+ switch (randomColor) {
+   case 'orange':
+   console.log('the color is orange');
+   break;
+   case  'blue':
+   console.log('the color is blue');
+   break; 
+   case  'yellow':
+   console.log('the color is yellow');
+   break;
+   case  'red':
+   console.log('the color is red');
+   break;
+   case  'purple':
+   console.log('the color is purple');
+   break;
+   case  'green':
+   console.log('the color is green');
+   break; 
+   case  'black':
+   console.log('the color is black');
+   break;
+   case  'white':
+   console.log('the color is white');
+   break; 
+   case  'pink':
+   console.log('the color is pink');
+   break; 
+   default :
+   console.log('no color found');
+ } 
